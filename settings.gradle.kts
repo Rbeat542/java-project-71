4c9e08ea285ff0d297f9873cceac45a6673f7327
Alexander Makevnin <a.makevnin@gmail.com>
2025-01-02 13:16:55 +0300

    settings update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@
 
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    //id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "App"
-//include("app")
+include("app")
