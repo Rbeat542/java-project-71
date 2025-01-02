441d2db20c38f703322feec69dcee0c73a5f31e9
Alexander Makevnin <a.makevnin@gmail.com>
2025-01-02 19:47:14 +0300

    configs updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-
 plugins {
     application
+    id("java")
     checkstyle
     jacoco
-    id("java")
-    id("checkstyle")
 }
 
 group = "hexlet.code"
@@ -14,24 +12,29 @@ application {
     mainClass = "hexlet.code.App"
 }
 
-repositories { mavenCentral() }
+repositories {
+    mavenCentral()
+}
 
 dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("info.picocli:picocli:4.7.6")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformats-text:2.18.2")
-    implementation("org.yaml:snakeyaml:2.3")
-    compileOnly("info.picocli:picocli-codegen:4.7.6")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+    }
 }
 
 tasks.jacocoTestReport {
     reports {
-        xml.required.set(true)
+        xml.required = true
+        html.required = true
     }
 }
